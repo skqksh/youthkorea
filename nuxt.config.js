6c9e118d2e9284cb57b4a3f3976c12767787c063
prolsh <prolsh@hsit.co.kr>
2018-07-28 01:46:03 +0900

    환경변수 설정 수정

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,13 @@
 const pkg = require('./package')
 const bodyParser = require('body-parser')
 
+const dbconn = {
+  baseUrl:
+    process.env.YOUTHKOREA_BASEURL || 'https://nuxt-blog-b6bb5.firebaseio.com',
+  fbAPIKey:
+    process.env.YOUTHKOREA_FBAPIKEY || 'AIzaSyBJgWepzRa5Ae1j3y2z47hb_-bt623XOgs'
+}
+
 module.exports = {
   mode: 'universal',
 
@@ -9,7 +16,8 @@ module.exports = {
    */
   head: {
     title: pkg.name,
-    meta: [{
+    meta: [
+      {
         charset: 'utf-8'
       },
       {
@@ -22,10 +30,11 @@ module.exports = {
         content: pkg.description
       }
     ],
-    link: [{
+    link: [
+      {
         rel: 'icon',
         type: 'image/x-icon',
-        href: '/favicon.ico',
+        href: '/favicon.ico'
       },
       {
         rel: 'stylesheet',
@@ -42,7 +51,7 @@ module.exports = {
   loading: {
     color: '#75a9ff',
     height: '3px',
-    duration: 5000,
+    duration: 5000
   },
   //상단의 mode 가 spa인 경우, 페이지가 로딩되는 동안 보여줄 로딩바를 설정할 수 있음
   //https://nuxtjs.org/api/configuration-loading-indicator/
@@ -55,18 +64,13 @@ module.exports = {
   /*
    ** Global CSS
    */
-  css: [
-    '~/assets/styles/main.css'
-  ],
+  css: ['~/assets/styles/main.css'],
 
   /*
    ** Plugins to load before mounting the App
    전역 component를 등록함으로써, 페이지별로 import할 필요가 없게됨
    */
-  plugins: [
-    '~plugins/core-components.js',
-    '~plugins/date-filter.js',
-  ],
+  plugins: ['~plugins/core-components.js', '~plugins/date-filter.js'],
 
   /*
    ** Nuxt.js modules
@@ -80,8 +84,8 @@ module.exports = {
     'bootstrap-vue/nuxt'
   ],
   axios: {
-    baseURL: process.env.BASE_URL || 'https://nuxt-blog-b6bb5.firebaseio.com',
-    credentials: false,
+    baseURL: dbconn.baseUrl,
+    credentials: false
   },
 
   /*
@@ -91,25 +95,21 @@ module.exports = {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {
-
-    }
-
+    extend(config, ctx) {}
   },
 
   //클라이언트와 서버 측면에서 공유할 환경 변수들
   //https://ko.nuxtjs.org/api/configuration-env#env-%ED%94%84%EB%A1%9C%ED%8D%BC%ED%8B%B0
   env: {
-    baseUrl: process.env.BASE_URL || 'https://nuxt-blog-b6bb5.firebaseio.com',
-    fbAPIKey: 'AIzaSyBJgWepzRa5Ae1j3y2z47hb_-bt623XOgs'
+    baseUrl: dbconn.baseUrl,
+    fbAPIKey: dbconn.fbAPIKey
   },
+
+  dev: true,
 
   router: {
     middleware: 'log'
   },
 
-  serverMiddleware: [
-    bodyParser.json(),
-    '~/api'
-  ]
+  serverMiddleware: [bodyParser.json(), '~/api']
 }
