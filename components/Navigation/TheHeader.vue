a2fadaea08ce92b591f7b531a3362090312557f5
DESKTOP-Q4UMOJL\blue <blue@test.com>
2018-08-11 14:31:19 +0900

    로그인되지 않았을시 로그아웃메뉴 나오지 않도록 함

--- components/Navigation/TheHeader.vue
+++ components/Navigation/TheHeader.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
-    <!-- 
-        네비게이션 참고 url : 
+    <!--
+        네비게이션 참고 url :
         https://github.com/vuetifyjs/vuetifyjs.com/blob/master/src/examples/layouts/complex.vue
-        
+
        -->
 
     <Drawer :items="loadedMenus" isRight :drawer="drawerRight" class="hidden-md-and-up" />
@@ -17,13 +17,13 @@
 
       <v-toolbar-items class="hidden-sm-and-down">
         <v-btn flat v-for="(menu, index) in loadedMenus" :key="index" @click="clickMenu(menu.id);selectedMainMenu=menu.id">
-          {{menu.name}} 
+          {{menu.name}}
         </v-btn>
 
       </v-toolbar-items>
       <v-tabs v-if="subMenuNav.length>0&&$vuetify.breakpoint.mdAndUp" slot="extension" color="transparent" centered slider-color="yellow">
         <template v-for="subMenu in subMenuNav">
-          <v-menu v-if="subMenu.children.length>0"  class="v-tabs__div"  offset-y right :key="subMenu.id">
+          <v-menu v-if="subMenu.children.length>0" class="v-tabs__div" offset-y right :key="subMenu.id">
             <v-btn flat slot="activator" @click="clickMenu(subMenu.id)">
               {{subMenu.name}}
               <v-icon>arrow_drop_down</v-icon>
@@ -46,119 +46,125 @@
 </template>
 
 <script>
-import Drawer from '@/components/Navigation/Drawer.vue'
-import CONST from '@/plugins/const.js'
+  import Drawer from '@/components/Navigation/Drawer.vue'
+  import CONST from '@/plugins/const.js'
 
-export default {
-  name: 'TheHeader',
-  props: {
-    isAdmin: {
-      type: Boolean
-    }
-  },
-  components: {
-    Drawer
-  },
-  data: () => ({
-    drawerRight: false,
-    drawer: true,
-    selectedMainMenu: '',
-    menus: [],
-    loadedMenus: [],
-    adminNavList: [
-      {
-        icon: 'web',
-        click: '/admin',
-        name: '관리자메인'
+  export default {
+    name: 'TheHeader',
+    props: {
+      isAdmin: {
+        type: Boolean
+      }
+    },
+    components: {
+      Drawer
+    },
+    data: () => ({
+      drawerRight: false,
+      drawer: true,
+      selectedMainMenu: '',
+      menus: [],
+      loadedMenus: []
+
+    }),
+    methods: {
+      moveTo(path) {
+        this.$router.push(path)
       },
-      {
-        icon: 'vertical_split',
-        click: '/admin/setting/menu',
-        name: '메뉴 설정'
+      onLogout() {
+        this.$store.dispatch('logout')
+        this.$router.push('/admin/auth')
       },
-      {
-        icon: 'view_comfy',
-        click: '/admin/setting/category',
-        name: '카테고리 설정'
+      navTo(val) {
+        if (val === 'onLogout') {
+          if (confirm('로그아웃 하시겠습니까?')) {
+            this.onLogout()
+          }
+        } else {
+          this.moveTo(val)
+        }
       },
-      {
-        icon: 'fas fa-clipboard-list',
-        name: '게시판 관리',
-        children: [
-          {
-            icon: 'far fa-edit',
-            click: '/admin/new-post',
-            name: '새글 작성'
+      clickMenu(id) {
+        let theMenu = this.menus.find(x => x.id === id)
+        if (theMenu.menuType === this.CONST.MENUTYPE.SINGLE) {
+          this.$router.push('/menu/page/' + id)
+        } else if (theMenu.menuType === this.CONST.MENUTYPE.MULTI) {
+          this.$router.push('/menu/board/' + id)
+        }
+      }
+    },
+    computed: {
+      subMenuNav() {
+        if (this.selectedMainMenu) {
+          return this.menus.filter(x => x.parentId === this.selectedMainMenu)
+        }
+        return []
+      },
+      adminNavList() {
+        var list = [{
+            icon: 'web',
+            click: '/admin',
+            name: '관리자메인'
           },
           {
-            icon: 'fas fa-edit',
-            click: '/admin/post-list',
-            name: '게시글목록 / 수정'
-          }
-        ]
-      },
-      {
-        icon: 'far fa-newspaper',
-        name: '페이지 관리',
-        children: [
+            icon: 'vertical_split',
+            click: '/admin/setting/menu',
+            name: '메뉴 설정'
+          },
           {
-            icon: 'fas fa-edit',
-            click: '/admin/page-list',
-            name: '페이지목록 / 수정'
+            icon: 'view_comfy',
+            click: '/admin/setting/category',
+            name: '카테고리 설정'
+          },
+          {
+            icon: 'fas fa-clipboard-list',
+            name: '게시판 관리',
+            children: [{
+                icon: 'far fa-edit',
+                click: '/admin/new-post',
+                name: '새글 작성'
+              },
+              {
+                icon: 'fas fa-edit',
+                click: '/admin/post-list',
+                name: '게시글목록 / 수정'
+              }
+            ]
+          },
+          {
+            icon: 'far fa-newspaper',
+            name: '페이지 관리',
+            children: [{
+              icon: 'fas fa-edit',
+              click: '/admin/page-list',
+              name: '페이지목록 / 수정'
+            }]
           }
         ]
-      },
-      {
-        icon: 'exit_to_app',
-        click: 'onLogout',
-        name: '로그아웃'
-      }
-    ]
-  }),
-  methods: {
-    moveTo(path) {
-      this.$router.push(path)
-    },
-    onLogout() {
-      this.$store.dispatch('logout')
-      this.$router.push('/admin/auth')
-    },
-    navTo(val) {
-      if (val === 'onLogout') {
-        if (confirm('로그아웃 하시겠습니까?')) {
-          this.onLogout()
+        if (this.$store.getters.isAuthenticated) {
+          list.push({
+            icon: 'exit_to_app',
+            click: 'onLogout',
+            name: '로그아웃'
+          })
         }
-      } else {
-        this.moveTo(val)
+        return list
       }
     },
-    clickMenu(id) {
-      let theMenu = this.menus.find(x => x.id === id)
-      if (theMenu.menuType === this.CONST.MENUTYPE.SINGLE) {
-        this.$router.push('/menu/page/' + id)
-      } else if (theMenu.menuType === this.CONST.MENUTYPE.MULTI) {
-        this.$router.push('/menu/board/' + id)
-      }
+    mounted() {},
+    created() {
+      this.menus = this.$store.getters.loadedMenus.sort((a, b) =>
+        this.CONST.sortFunc(a, b, 'order', true)
+      )
+      this.loadedMenus = this.CONST.unflatten(this.$store.getters.loadedMenus)
+
     }
-  },
-  computed: {
-    subMenuNav() {
-      if (this.selectedMainMenu) {
-        return this.menus.filter(x => x.parentId === this.selectedMainMenu)
-      }
-      return []
-    }
-  },
-  mounted() {},
-  created() {
-    this.menus = this.$store.getters.loadedMenus.sort((a, b) =>
-      this.CONST.sortFunc(a, b, 'order', true)
-    )
-    this.loadedMenus = this.CONST.unflatten(this.$store.getters.loadedMenus)
   }
-}
+
 </script>
 
 
 <style scoped>
+
+
 </style>
